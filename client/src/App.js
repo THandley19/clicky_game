36c7185009c7ae92b355417623cc6aab2bfbb3b2
Trevor <marketing@southernoaksinnstaug.com>
2020-06-27 09:55:09 -0400

    updated file structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,74 @@
-import React from "react";
-import MainGame from "./components/Main";
+import React, { Component } from "react";
 import "./App.css";
+import Jumbotron from "./components/Jumbotron/";
+import Cards from "./components/Cards/";
+import { Container, Row, Col } from "./components/Grid/index";
 
-function App() {
-  return (
-    <div>
-      <MainGame />
-    </div>
-  );
+export default class App extends Component {
+  state = {
+    userScore: 0,
+    images,
+    clickImages: [],
+    message: "",
+  };
+
+  handleSelectedItem = (id) => {
+    const clickedImages = this.state.clickImages;
+    if (clickedImages.includes(id)) {
+      return this.setState({
+        clickImages: [],
+        userScore: 0,
+        message: "Game Over! You lost. Click to play again!",
+      });
+    } else {
+      clickedImages.push(id);
+    }
+    if (clickedImages.length === 12) {
+      this.setState({
+        score: 12,
+        status: "You Won! Click to play again!",
+        clickImages: [],
+      });
+    }
+
+    this.setState({
+      images,
+      clickImages: this.state.clickImages,
+      userScore: this.state.clickImages.length,
+      message: "You guessed Correctly!",
+    });
+
+    for (let i = images.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      [images[i], images[j]] = [images[j], images[i]];
+    }
+  };
+  render() {
+    return (
+      <Container>
+        <Jumbotron
+          title="NFL Clicky Game"
+          userScore={this.state.userScore}
+          message={this.state.message}
+        />
+        <Row>
+          {this.state.images.map((images) => {
+            return (
+              <Col size="md-3 sm-6">
+                <Cards
+                  key={images.id}
+                  handleSelectedItem={this.handleSelectedItem}
+                  id={images.id}
+                  image={images.img}
+                  clicked={images.clicked}
+                ></Cards>
+              </Col>
+            );
+          })}
+        </Row>
+      </Container>
+    );
+  }
 }
 
 export default App;
